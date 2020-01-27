d07b78e5c55cbafb1b91e0542730ffc59e00600e
SiegfriedWagner <mateus.chojnowski@gmail.com>
2020-01-27 15:40:42 +0100

    updated cli scraper

--- CLIScraper/Program.cs
+++ CLIScraper/Program.cs
@@ -12,14 +12,14 @@ namespace CLIScraper
 {
     class Options
     {
-        [Option(shortName: 's', longName: "search", Required = true, HelpText = "Search phrase.")]
-        public string SearchPhrase { get; set; }
         [Option(shortName: 'o', longName: "output_dir", Required = true, HelpText = "Output directory")]
         public string ResultDirectory { get; set; }
         [Option(shortName: 'n', longName: "number", Required = true, HelpText = "Number of images to download")]
         public ushort NumberOfImages { get; set; }
         [Option(shortName: 'a', longName:"attempts", Required = false, Default = (ushort) 5, HelpText = "Number of attempts of getting search result")]
         public ushort Attempts { get; set; }
+        [Value(0, HelpText = "Phrases to search")]
+        public IEnumerable<string> SearchPhrases { get; set; }
     }
 
     class Program
@@ -34,29 +34,36 @@ namespace CLIScraper
                     Directory.CreateDirectory(o.ResultDirectory);
                 PageLoader pageLoader = PageLoader.GetPageLoader();
                 List<Func<string, Task<ImageSearchResult>>> imageSearchResultFuncs = null;
-                for (int i = 0; i < o.Attempts; i++)
+                foreach (var SearchPhrase in o.SearchPhrases)
                 {
-                    var task = pageLoader.LoadPageAsync($@"https://www.google.com/search?q={o.SearchPhrase}&source=lnms&tbm=isch");
-                    task.Wait();
-                    var imageUrlGenerator = GoogleImageDOMParser.GetUrls(task.Result);
-                    imageSearchResultFuncs = imageUrlGenerator.Take(o.NumberOfImages).ToList();
-                    if (imageSearchResultFuncs.Count > 0)
-                        break;
+                    var OutputDirectory = Path.Combine(o.ResultDirectory, SearchPhrase);
+                    Directory.CreateDirectory(OutputDirectory);
+                    for (int i = 0; i < o.Attempts; i++)
+                    {
+                        var task = pageLoader.LoadPageAsync($@"https://www.google.com/search?q={SearchPhrase}&source=lnms&tbm=isch");
+                        task.Wait();
+                        var imageUrlGenerator = GoogleImageDOMParser.GetUrls(task.Result);
+                        imageSearchResultFuncs = imageUrlGenerator.Take(o.NumberOfImages).ToList();
+                        if (imageSearchResultFuncs.Count > 0)
+                            break;
+                    }
+                    if (imageSearchResultFuncs == null)
+                        throw new InvalidDataException($"After {o.Attempts} attempts no search results was found!");
+                    var imageSearchResultTasks = imageSearchResultFuncs
+                        .AsParallel()
+                        .Select(m => m(OutputDirectory))
+                        .ToList();
+                    var a = Parallel.ForEach(imageSearchResultTasks, (imageResultTask) =>
+                    {
+                        Stopwatch watch = new Stopwatch();
+                        watch.Start();
+                        imageResultTask.Start();
+                        imageResultTask.Wait();
+                        watch.Stop();
+                        Console.WriteLine($"Finished downloading {imageResultTask.Result.Name} in {watch.ElapsedMilliseconds}ms");
+                    });
+                    
                 }
-                if (imageSearchResultFuncs == null)
-                    throw new InvalidDataException($"After {o.Attempts} attempts no search results was found!");
-                var imageSearchResultTasks = imageSearchResultFuncs
-                    .AsParallel()
-                    .Select(m => m(o.ResultDirectory))
-                    .ToList();
-                var a = Parallel.ForEach(imageSearchResultTasks, (imageResultTask) =>
-                {
-                    Stopwatch watch = new Stopwatch();
-                    watch.Start();
-                    imageResultTask.Wait();
-                    watch.Stop();
-                    Console.WriteLine($"Finished downloading {imageResultTask.Result.Name} in {watch.ElapsedMilliseconds}ms");
-                });
                 Console.WriteLine("All images were downloaded succesfully");
                 Console.ReadKey();
             });
